b86892a15768fd09ccfb178cdf7b8d3d75e27c4c
Arianit Gashi <gashiarianit2003@gmail.com>
2024-05-17 22:12:27 +0200

    added-login-view

--- public/script.js
+++ public/script.js
@@ -17,3 +17,25 @@ function register() {
         }
     });
 }
+
+function login() {
+    const email = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+
+    fetch('/api/users/login', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ email, password })
+    })
+    .then(response => response.json())
+    .then(data => {
+        if (data.token) {
+            localStorage.setItem('token', data.token);
+            alert('Login successful');
+            window.location.href = '/compose.html';
+            
+        } else {
+            alert('Login failed');
+        }
+    });
+}
